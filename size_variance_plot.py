d1ddbfc92281ca86378c57c3a2cf65e9bb0031f7
jack <jack.lynam.19@cnu.edu>
2023-03-03 13:14:57 -0500

    Created script to show correlation between dataset size and f1 variance across all language models

---
+++ size_variance_plot.py
@@ -0,0 +1,89 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy.stats as stats
+from scipy.optimize import curve_fit
+
+
+def linear_func(x, m, b):
+    return m * x + b
+
+
+def main():
+    # THESE NEED TO BE ALPHABETIZED
+    # Includes all datasets
+    # dataset_word_size = [291345, 922104, 1481246, 271175, 463200, 364446, 904414, 146791, 729228]
+    # dataset_sample_size = [18300, 42575, 127125, 13880, 19988, 43940, 12809, 7153, 33842]
+    
+    # Excludes BC7MED, class imbalanced twitter dataset
+    dataset_word_size = np.array([291345, 922104, 271175, 463200, 364446, 904414, 146791, 729228])
+    dataset_sample_size = np.array([18300, 42575, 13880, 19988, 43940, 12809, 7153, 33842])
+
+    file_path = os.path.join("results", "experiment_1_results", "final_results_master.tsv")
+    dtypes = {'dataset':str, 'lm_name':str, "micro_precision_av":float, "micro_precision_std":float, "micro_recall_av":float, "micro_recall_std":float, "micro_f1_av":float, "micro_f1_std":float, "macro_precision_av":float, "macro_precision_std":float, "macro_recall_av":float, "macro_recall_std":float, "macro_f1_av":float, "macro_f1_std":float}
+    df = pd.read_csv(file_path, header=0, sep='\t', dtype=dtypes)
+
+    # Dropping bc7med because its a major outlier
+    df.drop(df[df.dataset=='bc7med'].index, inplace=True)
+    var_by_dataset = df.groupby(['dataset']).var(numeric_only=True)
+    # THESE ARE ALPHABETIZED
+    variances = var_by_dataset['micro_f1_av'].tolist()
+
+    # Linear regression for the word count by variance
+    popt, pcov = curve_fit(linear_func, dataset_word_size, variances)
+    word_residuals = variances - linear_func(dataset_word_size, popt[0], popt[1])
+    word_ss_res = np.sum(word_residuals ** 2)
+    word_ss_tot = np.sum((variances - np.mean(variances))**2)
+
+    word_r_squared = 1 - (word_ss_res / word_ss_tot)
+
+    print("OPT params:", popt)
+    print("COVs:", pcov)
+    print("R^2:", word_r_squared)
+    print("-" * 50)
+
+    fig, ax = plt.subplots(figsize=[12,8])
+
+    ax.set_title("Dataset word count by variance", fontsize=20)
+    ax.set_xlabel("Number of words in dataset", fontsize=15)
+    ax.set_title("Variance in micro f1 scores", fontsize=15)
+    ax.scatter(dataset_word_size, variances, linewidth=5)
+    ax.plot(dataset_word_size, [linear_func(val, popt[0], popt[1]) for val in dataset_word_size], label=r"$r^{2}=$" + f"{word_r_squared:.4f}") 
+
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles, labels, loc='upper right', fontsize=15)
+
+    plt.tight_layout()
+    plt.savefig("word_count_variance.pdf", dpi=400)
+
+
+    # Linear regression for the sample count by variance
+    popt, pcov = curve_fit(linear_func, dataset_sample_size, variances)
+    sample_residuals = variances - linear_func(dataset_sample_size, popt[0], popt[1])
+    sample_ss_res = np.sum(sample_residuals ** 2)
+    sample_ss_tot = np.sum((variances - np.mean(variances))**2)
+
+    sample_r_squared = 1 - (sample_ss_res / sample_ss_tot)
+
+    print("OPT params:", popt)
+    print("COVs:", pcov)
+    print("R^2:", sample_r_squared)
+
+    fig, ax = plt.subplots(figsize=[12,8])
+
+    ax.set_title("Dataset sample count by variance", fontsize=20)
+    ax.set_xlabel("Number of samples in dataset", fontsize=15)
+    ax.set_title("Variance in micro f1 scores", fontsize=15)
+    ax.scatter(dataset_sample_size, variances, linewidth=5)
+    ax.plot(dataset_sample_size, [linear_func(val, popt[0], popt[1]) for val in dataset_sample_size], label=r"$r^{2}=$" + f"{sample_r_squared:.2f}")
+
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles, labels, loc='upper right', fontsize=15)
+    plt.tight_layout()
+    plt.savefig("sample_count_variance.pdf", dpi=400)
+
+
+if __name__=="__main__":
+    main()
+
