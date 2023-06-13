4c486ad27ee96a25fbd6f225563b9c43ef8b11d2
Sam Henry <samuel.henry@cnu.edu>
2023-06-13 16:59:40 -0400

    added_run_all_experiments

---
+++ run_all_experiments.py
@@ -0,0 +1,9 @@
+import experiment1
+import experiment2
+import experiment3a
+import experiment3b
+
+experiment1.run_experiment_1()
+experiment2.run_experiment_2()
+experiment3a.run_experiment_3a()
+experiment3b.run_experiment_3b()
